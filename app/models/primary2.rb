d28c935338f48776bb7610515e948f626276c792
BhadreshC <wc.bhadresh@gmail.com>
2021-03-17 09:56:20 +0530

    multiple db user and post both have separate db

---
+++ app/models/primary2.rb
@@ -0,0 +1,5 @@
+class Primary2 < ActiveRecord::Base
+  self.abstract_class = true
+  # connects_to database: { writing: :primary2, reading: :primary2 }
+  connects_to database: { writing: :primary2, reading: :primary_replica_2 }
+end
---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < Primary2
+  # connects_to database: { writing: :primary2, reading: :primary2 }
+end
