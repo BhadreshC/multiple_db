b56d619dba4e274c2f2aeb61236cb6c9155cf76d
BhadreshC <wc.bhadresh@gmail.com>
2021-04-07 15:18:45 +0530

    add puts

--- app/models/primary2.rb
+++ app/models/primary2.rb
@@ -2,4 +2,7 @@ class Primary2 < ActiveRecord::Base
   self.abstract_class = true
   # connects_to database: { writing: :primary2, reading: :primary2 }
   connects_to database: { writing: :primary2, reading: :primary_replica_2 }
+  puts "------------------"
+  puts "------------------"
+  puts "------------------"
 end
